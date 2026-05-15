686ed2215c7c91f320f02e2ccdc31d2d22378436
ankityadavworkin <ankit.yadav.workin@gmail.com>
2026-05-15 15:39:14 +0530

    Add to car script added - client dashboard

--- tests/EndToEndClientApp.spec.js
+++ tests/EndToEndClientApp.spec.js
@@ -36,12 +36,30 @@ test('SignUp Dashboard - Rahul shetty academy Login Page Pracytice', async ({pag
 const username = page.locator("#userEmail");
 const password = page.locator("#userPassword");
 const signInBtn = page.locator("#login"); 
+const productName = "ZARA COAT 3";
 await page.goto("https://rahulshettyacademy.com/client");
 await username.fill("ranveeryadav5555@gmail.com");
 await password.fill("Demo@123");
 await signInBtn.click();
 await page.waitForLoadState('networkidle'); 
-await page.locator(".card-body b").first().waitFor;
-const titles = await page.locator(".card-body b").allTextContents();
-console.log(titles);
+const productsTitlesParent = page.locator(".card-body");
+const productsTitlesLocator = page.locator(".card-body b");
+await productsTitlesLocator.first().waitFor(); // This will wait for the first element located by ".card-body b" to be visible on the page before proceeding with the next steps.
+const titles = await productsTitlesLocator.allTextContents(); // This will return an array of all the text contents of the elements located by ".card-body b"
+console.log("All product titles: " + titles);
+const productsCount = await productsTitlesLocator.count(); // This will return the number of elements located by ".card-body b", which is the count of products displayed on the page.
+console.log("Number of products: " + productsCount);
+
+for(let i=0; i<productsCount; i++){
+   if(await productsTitlesLocator.nth(i).textContent() === productName){
+       console.log("Product found: " + productName);
+       await productsTitlesParent.nth(i).locator("text= Add To Cart").click(); // This will click on the "Add To Cart" button for the product that matches the productName.
+       break; // Exit the loop once the product is found and added to the cart.
+   }
+}
+await page.locator("[routerlink*='cart']").click(); // This will click on the cart link to navigate to the cart page.
+// const isProductVisible = await page.locator("h3:has-text('ZARA COAT 3')").isVisible(); // This will wait for the product "ZARA COAT 3" to be visible in the cart page before proceeding with the next steps.
+const isProductVisible = await page.locator("h3:has-text('"+productName+"')").isVisible(); // This will wait for the product "ZARA COAT 3" to be visible in the cart page before proceeding with the next steps.
+expect(isProductVisible).toBeTruthy(); // This will assert that the product "ZARA COAT 3" is visible in the cart page, confirming that it was successfully added to the cart.
+await page.waitForTimeout(3000);
 });
